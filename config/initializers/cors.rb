1902742441e2e7ac12d66f1e2a8ea4fb39c515b4
tpham13 <tpham0123@gmail.com>
2021-02-16 12:08:34 -0800

    added bcrypt and cors

---
+++ config/initializers/cors.rb
@@ -0,0 +1,13 @@
+# What does it say here: we're going to insert a level of middleware here and 
+# we're going to use cors module to do it and all the rules we're going to put inside here is going to be intercept by Rails config
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do 
+        # this allow whatever domain you want here
+        origins "*"
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        # crendentials allow you to put the headers back and forth front end and back end
+credentials: false
+    end
+ 
+end
